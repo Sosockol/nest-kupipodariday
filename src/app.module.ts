d8ca881058ca9e7354441d8fb7b1e98d186062bc
d.bolokhontsev <bolokhontsev.main@gmail.com>
2025-10-01 18:39:07 +0300

    feat: .env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,40 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const getEnvFilePath = () => {
+  const env = process.env.NODE_ENV || 'development';
+  const envFiles = [
+    `.env.${env}.local`,
+    `.env.local`,
+    `.env.${env}`,
+    '.env',
+  ];
+  return envFiles;
+};
+
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST || 'localhost',
-      port: Number(process.env.DB_PORT) || 5432,
-      username: process.env.DB_USERNAME || 'postgres',
-      password: process.env.DB_PASSWORD || 'postgres',
-      database: process.env.DB_NAME || 'kupipodariday',
-      entities: [`${__dirname}/**/*.entity{.ts,.js}`],
-      synchronize: process.env.NODE_ENV !== 'production',
-      logging: process.env.NODE_ENV === 'development',
+    ConfigModule.forRoot({
+      envFilePath: getEnvFilePath(),
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST', 'localhost'),
+        port: configService.get('DB_PORT', 5432),
+        username: configService.get('DB_USERNAME', 'postgres'),
+        password: configService.get('DB_PASSWORD', 'postgres'),
+        database: configService.get('DB_NAME', 'kupipodariday'),
+        entities: [`${__dirname}/**/*.entity{.ts,.js}`],
+        synchronize: configService.get('NODE_ENV') !== 'production',
+        logging: configService.get('NODE_ENV') === 'development',
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
