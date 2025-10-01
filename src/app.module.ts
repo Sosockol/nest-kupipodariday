c67b3a7a95ddbcf3b38a519504eff5ba2a545717
d.bolokhontsev <bolokhontsev.main@gmail.com>
2025-10-01 18:32:59 +0300

    feat: connect db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: Number(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD || 'postgres',
+      database: process.env.DB_NAME || 'kupipodariday',
+      entities: [`${__dirname}/**/*.entity{.ts,.js}`],
+      synchronize: process.env.NODE_ENV !== 'production',
+      logging: process.env.NODE_ENV === 'development',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
